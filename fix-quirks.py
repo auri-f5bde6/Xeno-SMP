a4a7649e1746874b217bfccbab18a26697cc16ea
auri <f5bde6@proton.me>
2026-08-03 18:03:37 +0100

    Please work this time

--- fix-quirks.py
+++ fix-quirks.py
@@ -3,12 +3,18 @@ import tomlkit
 from tomlkit.items import AbstractTable
 
 
-def delete(metadata):
+def fix(filename, metadata):
     for key in list(metadata.keys()):
         if key.startswith("x-prismlauncher-"):
             metadata.pop(key)
         elif isinstance(metadata[key], AbstractTable):
-            delete(metadata[key])
+            fix(filename, metadata[key])
+        elif key=="side" and metadata[key] not in ["client", "server", "both"]:
+            print(f"WARNING: {filename} have a invalid side of '{metadata[key]}', falling back to both")
+            metadata[key] = "both" # default to both
+        elif key=="url" and isinstance(metadata[key], str) and metadata[key].strip()=="":
+            print(f"WARNING: {filename} have empty url, deleting field")
+            metadata.pop(key)
 
 for mod in os.listdir("mods"):
     if mod.endswith(".pw.toml"):
@@ -16,6 +22,6 @@ for mod in os.listdir("mods"):
         with open(os.path.join("mods", mod), "r") as f:
             text = f.read()
         metadata = tomlkit.parse(text)
-        delete(metadata)
+        fix(mod, metadata)
         with(open(os.path.join("mods", mod), "w")) as f:
             f.write(tomlkit.dumps(metadata))
